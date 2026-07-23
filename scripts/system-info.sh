470dc0518f751d05709484f271616647abbb9582
Carlos Blaise <c.hanspascalblaise@outlook.com>
2026-07-23 14:28:16 -0400

    Add system information script

---
+++ scripts/system-info.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "===== System Information ====="
+echo
+
+hostnamectl
+
+echo
+echo "===== Disk Usage ====="
+df -h
+
+echo
+echo "===== Memory Usage ====="
+free -h
+
+echo
+echo "===== CPU ====="
+
